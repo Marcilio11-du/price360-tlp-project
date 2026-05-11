f484df5a734d29be5e31d428e02af812982cbd9e
enidev <neiledson16@gmail.com>
2026-05-11 01:17:56 +0100

    feat: create a function to search for a store by id

--- src/models/storeModels.js
+++ src/models/storeModels.js
@@ -1,10 +1,17 @@
 const db = require('../config/db');
+const { create } = require('./categoryModels');
 
 const storeModel = {
     findAll: async () => {
         const [rows] = await db.execute('SELECT * FROM Loja ORDER BY nome ASC');
         return rows;
-    }
+    },
+
+    findById: async (id) => {
+        const [rows] = await db.execute('SELECT * FROM Loja WHERE id = ?', [id]);
+        return rows[0];
+    },
+
 };
 
 module.exports = storeModel;
