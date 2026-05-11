5489898e3cd61f714c054c321232eef68eb9ae03
enidev <neiledson16@gmail.com>
2026-05-12 00:04:02 +0100

    feat: add all endpoints and implemente softDelete function

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryControllers');
 
 router.get('/', categoryController.getAllCategories);
+router.get('/all', categoryController.getAllCategoriesIncludingDeleted);
+router.get('/:id', categoryController.getCategoryById);
 router.post('/', categoryController.createCategory);
+router.put('/:id', categoryController.updateCategory);
+router.delete('/:id', categoryController.softDeleteCategory);
+router.patch('/:id/restore', categoryController.restoreCategory);
+router.delete('/:id/hard', categoryController.hardDeleteCategory);
 
 module.exports = router;
