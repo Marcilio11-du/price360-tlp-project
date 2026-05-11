8110ad7b52aee239f6f8fc2ef7a4a55ffa75ebbc
enidev <neiledson16@gmail.com>
2026-05-12 00:31:48 +0100

    feat: finish store's crud

--- src/models/storeModels.js
+++ src/models/storeModels.js
@@ -6,22 +6,44 @@ const storeModel = {
         return rows;
     },
 
+    findAllActives: async () => {
+        const [rows] = await db.execute('SELECT * FROM Loja WHERE deleted_at IS NULL ORDER BY nome ASC');
+        return rows;
+    },
+
     findById: async (id) => {
         const [rows] = await db.execute('SELECT * FROM Loja WHERE id = ?', [id]);
         return rows[0];
     },
 
     create: async (nome, endereco) => {
-        const [result] = await db.execute('INSERT INTO Loja (nome, endereco) VALUES (?, ?)', [nome, endereco])
-        return result.insertId;
+        const [result] = await db.execute('INSERT INTO Loja (nome, endereco, created_at, updated_at) VALUES (?, ?, NOW(), NOW())', [nome, endereco]);
+        const [rows] = await db.execute('SELECT * FROM Loja WHERE id = ?', [result.insertId]);
+        return rows[0];
     },
 
     update: async (id, nome, endereco) => {
-        await db.execute('UPDATE Loja SET nome = ?, endereco = ? WHERE id = ?', [nome, endereco, id])
+        await db.execute('UPDATE Loja SET nome = ?, endereco = ?, updated_at = NOW() WHERE id = ?', [nome, endereco, id]);
+        const [rows] = await db.execute('SELECT * FROM Loja WHERE id = ?', [id]);
+        return rows[0];
+    },
+
+    softDelete: async (id) => {
+        await db.execute('UPDATE Loja SET deleted_at = NOW(), updated_at = NOW() WHERE id = ?', [id]);
+        const [rows] = await db.execute('SELECT * FROM Loja WHERE id = ?', [id]);
+        return rows[0];
+    },
+
+    restore: async (id) => {
+        await db.execute('UPDATE Loja SET deleted_at = NULL, updated_at = NOW() WHERE id = ?', [id]);
+        const [rows] = await db.execute('SELECT * FROM Loja WHERE id = ?', [id]);
+        return rows[0];
     },
 
     delete: async (id) => {
-        await db.execute('DELETE FROM Loja WHERE id = ?', [id])
+        const [rows] = await db.execute('SELECT * FROM Loja WHERE id = ?', [id]);
+        await db.execute('DELETE FROM Loja WHERE id = ?', [id]);
+        return rows[0];
     }
 
 };
